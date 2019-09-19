fdb8f56a42f72b0ac7e940599cf6888fde7eb88b
dafo90 <41897414+dafo90@users.noreply.github.com>
2019-09-19 22:13:01 +0200

    Added layouts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import { Typography } from "@material-ui/core";
 import React from 'react';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import NotFound from './components/404';
+import AboutMe from './components/aboutme';
+import Contact from './components/contact';
+import Projects from './components/projects';
+import Resume from './components/resume';
 
 function App() {
-  return (
-    <Typography variant="h4" gutterBottom component="h2">Contact</Typography>
-  );
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" render={() => <Redirect to="/aboutme" />} />
+                <Route exact path="/aboutme" component={AboutMe} />
+                <Route exact path="/resume" component={Resume} />
+                <Route exact path="/contact" component={Contact} />
+                <Route exact path="/projects" component={Projects} />
+                <Route component={NotFound} />
+            </Switch>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/components/404.js
@@ -0,0 +1,10 @@
+import { Typography } from "@material-ui/core";
+import React from 'react';
+
+function NotFound() {
+  return (
+    <Typography variant="h4" gutterBottom component="h2">404</Typography>
+  );
+}
+
+export default NotFound;
