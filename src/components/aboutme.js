bad2f3c53afa934398ca191661bd64a5c2724084
dafo90 <41897414+dafo90@users.noreply.github.com>
2019-09-20 16:18:56 +0200

    Added theme

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,10 +1,12 @@
-import { Typography } from "@material-ui/core";
+import { Typography } from '@material-ui/core';
 import React from 'react';
 
 function AboutMe() {
-  return (
-    <Typography variant="h4" gutterBottom component="h2">AboutMe</Typography>
-  );
+    return (
+        <Typography variant="h4" gutterBottom component="h2">
+            AboutMe
+        </Typography>
+    );
 }
 
 export default AboutMe;
--- src/components/resume.js
+++ src/components/resume.js
@@ -1,10 +1,12 @@
-import { Typography } from "@material-ui/core";
+import { Typography } from '@material-ui/core';
 import React from 'react';
 
 function Resume() {
-  return (
-    <Typography variant="h4" gutterBottom component="h2">Resume</Typography>
-  );
+    return (
+        <Typography variant="h4" gutterBottom component="h2">
+            Resume
+        </Typography>
+    );
 }
 
 export default Resume;
