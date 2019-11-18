fddc3ef53ecba9897525d42f69e24220cc0305b7
dafo90 <41897414+dafo90@users.noreply.github.com>
2019-11-18 23:23:53 +0100

    Socials on contact header

--- src/components/layouts/contact/Contact.js
+++ src/components/layouts/contact/Contact.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import { Paper } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import LayoutHeader from '../LayoutHeader';
 import HeaderTitle from '../../common/HeaderTitle';
+import SocialButtonsBar from '../../common/SocialButtonsBar';
 import LayoutBody from '../LayoutBody';
+import socials from '../../../configurations/socials';
+
+const useStyles = makeStyles(theme => ({
+    socialsBar: {
+        paddingTop: theme.spacing(2)
+    }
+}));
 
 const Contact = () => {
+    const classes = useStyles();
     return (
         <React.Fragment>
             <LayoutHeader>
-                <HeaderTitle title="Contact" subtitle="Get in touch" imgSrc="/logos/mail.svg" imgAlt="Contact" />
+                <HeaderTitle
+                    title="Contact"
+                    subtitle={
+                        <div>
+                            <div>Get in touch</div>
+                            <SocialButtonsBar socials={socials} iconSize="22px" className={classes.socialsBar} />
+                        </div>
+                    }
+                    imgSrc="/logos/mail.svg"
+                    imgAlt="Contact"
+                />
             </LayoutHeader>
             <LayoutBody>
                 <Paper />
