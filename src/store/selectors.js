e47dcf53ec81e176522f944ce23c9803d2033148
dafo90 <41897414+dafo90@users.noreply.github.com>
2021-01-11 20:57:38 +0100

    Fix selector and added redux logo

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -9,7 +9,7 @@ export const githubSelector = (state) => state.github;
 export const githubConfigSelector = (state) => state.config?.github || {};
 export const layoutsConfigSelector = (state) => state.config?.layouts || [];
 export const skillsConfigSelector = (state) => state.config?.skills || [];
-export const socialsConfigSelector = (state) => state.config?.socialsConfig || [];
+export const socialsConfigSelector = (state) => state.config?.socials || [];
 export const personalInformationConfigSelector = (state) => state.config?.personalInformation || {};
 export const notFoundConfigSelector = (state) => state.config?.notFound || {};
 export const footerConfigSelector = (state) => state.config?.footer || {};
